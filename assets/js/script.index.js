a69290a82e938963e1261f539c8c45a8f47eacac
giobasiliox <giovanabasilio.564@gmail.com>
2023-08-18 09:17:34 -0300

    first commit

---
+++ assets/js/script.index.js
@@ -0,0 +1,46 @@
+// const numbers = [1 ,2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// console.log(numbers);
+
+// numbers.forEach((number, index)=>{
+//     if (number % 2 == 0){
+//         console.log(`O número ${number} está na posição ${index}`);
+//     }
+// })
+
+class Car{
+    constructor(marca, modelo){
+        this.marca= marca;
+        this.model= modelo;
+    }
+}
+
+class Listacar{
+    constructor(){
+        this.cars= [];
+    }
+
+    adicionar(car){
+        this.cars.push(car);
+    }
+}
+
+const listaCars= new Listacar();
+
+function createCar(){
+    const marca= document.getElementById("nameMarca").value;
+    const modelo= document.getElementById("modelcar").value;
+
+    const car= new Car (marca, modelo);
+
+    listaCars.adicionar(car);
+
+    console.log(`O marca do carro é ${marca} e o modelo é ${modelo}.`);
+}
+
+
+// cars.forEach((car)=>{
+//     console.log(`Marca:${car.marca} 
+// Modelo: ${car.modelo}`)
+// }
+// )
